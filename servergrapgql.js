753d3effa656f69a1e83171096d17c6deab766d4
fimovol <fimovolpe@gmail.com>
2021-12-13 00:27:51 -0500

    ahahahahah

---
+++ servergrapgql.js
@@ -0,0 +1,73 @@
+var { buildSchema } = require('graphql');
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var axios = require('axios')
+const cors = require('cors')
+
+var schema = buildSchema(`
+  type Nota {
+    content: String
+    date: String
+    important: Boolean
+    id: String
+  }
+  type Mutation {
+    updateMessage(numDice: Int!): Int
+
+
+    updateNota(content: String!,important: Boolean):Nota
+  }
+  type Query {
+    rollDice(numDice: Int!, numSides: Int): [Int]
+
+
+    getNotaconid(id: String): Nota
+    getNota: [Nota]
+  }
+`);
+
+var root = {
+    updateMessage: ({numDice}) => {
+        return numDice;
+    },
+    rollDice: ({numDice, numSides}) => {
+        var output = [];
+        for (var i = 0; i < numDice; i++) {
+          output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+        }
+        return output;
+    },
+
+
+
+    getNota: async ()=>{
+        const {data:datos} = await axios.get('http://localhost:3002')
+        return datos
+    },
+    getNotaconid: async ({id})=>{
+        const {data:datos} = await axios.get(`http://localhost:3002/api/notes/${id}`)
+        return datos
+    },
+    updateNota: async ({content,important})=>{
+        const json = JSON.stringify({
+            content: content,
+            important: important
+        });
+        const {data:datos} = await axios.post('http://localhost:3002/api/notes', json, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        return datos
+    }
+}
+
+var app = express();
+app.use(cors())
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4001);
+console.log('Running a GraphQL API server at http://localhost:4001/graphql');
